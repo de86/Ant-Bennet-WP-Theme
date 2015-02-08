d0f346596d0716e6868547e72ae1d8c819cb7a59
de86 <daelliott86@gmail.com>
2015-02-08 14:37:19 +0000

    Removed incorrect enqueueing of jQuery from functions.php

--- functions.php
+++ functions.php
@@ -109,8 +109,6 @@ function anthony_bennett_scripts() {
 
 	wp_enqueue_script( 'anthony-bennett-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'anthony-bennett-jquery', get_template_directory_uri() . '/js/jquery.min.js', array(''), '20150208', true );
-
 	wp_enqueue_script( 'anthony-bennett-mainjs', get_template_directory_uri() . '/js/main.js', array('jquery'), '20150208', true );
 
 	wp_enqueue_script( 'anthony-bennett-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
